cdefa40d0ec76682a9215f3dae41ff51fb80d54a
Angelo Cabezas <ang_7jd@hotmail.com>
2022-11-24 01:27:39 -0500

    +Recipes

---
+++ TheMenu.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using TheMenu.Domain.Entities.Base;
+
+namespace TheMenu.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public IEnumerable<CategoryRecipe> CategoryRecipes { get; set; }
+    }
+}
---
+++ TheMenu.Domain/Entities/CategoryRecipe.cs
@@ -0,0 +1,10 @@
+﻿using TheMenu.Domain.Entities.Base;
+
+namespace TheMenu.Domain.Entities
+{
+    public class CategoryRecipe : BaseEntity
+    {
+        public Category Category { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
